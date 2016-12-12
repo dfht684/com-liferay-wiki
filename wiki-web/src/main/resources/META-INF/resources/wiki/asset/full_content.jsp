marcellustavares/liferay-portal

LPS-69400 update references for Wiki

modules/apps/collaboration/wiki/wiki-web/src/main/resources/META-INF/resources/wiki/asset/full_content.jsp
@@ -41,11 +41,11 @@ WikiPageDisplay pageDisplay = WikiPageLocalServiceUtil.getPageDisplay(wikiPage, 
 
 <%= pageDisplay.getFormattedContent() %>
 
-<liferay-ui:custom-attributes-available className="<%= WikiPage.class.getName() %>">
-	<liferay-ui:custom-attribute-list
+<liferay-custom-field-ui:custom-attribute-available className="<%= WikiPage.class.getName() %>">
+	<liferay-custom-field-ui:custom-attribute-list
 		className="<%= WikiPage.class.getName() %>"
 		classPK="<%= (wikiPage != null) ? wikiPage.getPrimaryKey() : 0 %>"
 		editable="<%= false %>"
 		label="<%= true %>"
 	/>
-</liferay-ui:custom-attributes-available>
+</liferay-custom-field-ui:custom-attribute-available>

modules/apps/collaboration/wiki/wiki-web/src/main/resources/META-INF/resources/wiki/edit_page.jsp
@@ -317,16 +317,16 @@ if (portletTitleBasedNavigation) {
 					</aui:fieldset>
 
 					<c:if test="<%= wikiPage != null %>">
-						<liferay-ui:custom-attributes-available className="<%= WikiPage.class.getName() %>">
+						<liferay-custom-field-ui:custom-attribute-available className="<%= WikiPage.class.getName() %>">
 							<aui:fieldset collapsed="<%= true %>" collapsible="<%= true %>" label="custom-fields">
-								<liferay-ui:custom-attribute-list
+								<liferay-custom-field-ui:custom-attribute-list
 									className="<%= WikiPage.class.getName() %>"
 									classPK="<%= (templatePage != null) ? templatePage.getPrimaryKey() : wikiPage.getPrimaryKey() %>"
 									editable="<%= true %>"
 									label="<%= true %>"
 								/>
 							</aui:fieldset>
-						</liferay-ui:custom-attributes-available>
+						</liferay-custom-field-ui:custom-attribute-available>
 					</c:if>
 
 					<c:if test="<%= (wikiPage == null) || wikiPage.isNew() %>">

modules/apps/collaboration/wiki/wiki-web/src/main/resources/META-INF/resources/wiki/view.jsp
@@ -304,14 +304,14 @@ if (portletTitleBasedNavigation) {
 
 						<%@ include file="/wiki/view_page_content.jspf" %>
 
-						<liferay-ui:custom-attributes-available className="<%= WikiPage.class.getName() %>">
-							<liferay-ui:custom-attribute-list
+						<liferay-custom-field-ui:custom-attribute-available className="<%= WikiPage.class.getName() %>">
+							<liferay-custom-field-ui:custom-attribute-list
 								className="<%= WikiPage.class.getName() %>"
 								classPK="<%= (wikiPage != null) ? wikiPage.getPrimaryKey() : 0 %>"
 								editable="<%= false %>"
 								label="<%= true %>"
 							/>
-						</liferay-ui:custom-attributes-available>
+						</liferay-custom-field-ui:custom-attribute-available>
 
 						<c:if test="<%= (wikiPage != null) && Validator.isNotNull(formattedContent) && (followRedirect || (redirectPage == null)) %>">
 							<div class="page-actions">

modules/apps/collaboration/wiki/wiki-web/src/main/resources/META-INF/resources/wiki_admin/page_info_panel.jsp
@@ -149,14 +149,14 @@ if (wikiPageInfoPanelDisplayContext.isSinglePageSelection()) {
 							/>
 						</c:if>
 
-						<liferay-ui:custom-attributes-available className="<%= WikiPage.class.getName() %>">
-							<liferay-ui:custom-attribute-list
+						<liferay-custom-field-ui:custom-attribute-available className="<%= WikiPage.class.getName() %>">
+							<liferay-custom-field-ui:custom-attribute-list
 								className="<%= WikiPage.class.getName() %>"
 								classPK="<%= wikiPage.getResourcePrimKey() %>"
 								editable="<%= false %>"
 								label="<%= true %>"
 							/>
-						</liferay-ui:custom-attributes-available>
+						</liferay-custom-field-ui:custom-attribute-available>
 
 						<%
 						AssetEntry wikiPageAssetEntry = AssetEntryLocalServiceUtil.fetchEntry(WikiPage.class.getName(), wikiPage.getPrimaryKey());

dfht684/com-liferay-wiki

wiki-web/src/main/resources/META-INF/resources/wiki/asset/full_content.jsp
@@ -41,11 +41,11 @@ WikiPageDisplay pageDisplay = WikiPageLocalServiceUtil.getPageDisplay(wikiPage, 
 
 <%= pageDisplay.getFormattedContent() %>
 
-<liferay-ui:custom-attributes-available className="<%= WikiPage.class.getName() %>">
-	<liferay-ui:custom-attribute-list
+<liferay-custom-field-ui:custom-attributes-available className="<%= WikiPage.class.getName() %>">
+	<liferay-custom-field-ui:custom-attribute-list
 		className="<%= WikiPage.class.getName() %>"
 		classPK="<%= (wikiPage != null) ? wikiPage.getPrimaryKey() : 0 %>"
 		editable="<%= false %>"
 		label="<%= true %>"
 	/>
-</liferay-ui:custom-attributes-available>
+</liferay-custom-field-ui:custom-attributes-available>

wiki-web/src/main/resources/META-INF/resources/wiki/edit_page.jsp
@@ -317,16 +317,16 @@ if (portletTitleBasedNavigation) {
 					</aui:fieldset>
 
 					<c:if test="<%= wikiPage != null %>">
-						<liferay-ui:custom-attributes-available className="<%= WikiPage.class.getName() %>">
+						<liferay-custom-field-ui:custom-attributes-available className="<%= WikiPage.class.getName() %>">
 							<aui:fieldset collapsed="<%= true %>" collapsible="<%= true %>" label="custom-fields">
-								<liferay-ui:custom-attribute-list
+								<liferay-custom-field-ui:custom-attribute-list
 									className="<%= WikiPage.class.getName() %>"
 									classPK="<%= (templatePage != null) ? templatePage.getPrimaryKey() : wikiPage.getPrimaryKey() %>"
 									editable="<%= true %>"
 									label="<%= true %>"
 								/>
 							</aui:fieldset>
-						</liferay-ui:custom-attributes-available>
+						</liferay-custom-field-ui:custom-attributes-available>
 					</c:if>
 
 					<c:if test="<%= (wikiPage == null) || wikiPage.isNew() %>">

wiki-web/src/main/resources/META-INF/resources/wiki/view.jsp
@@ -304,14 +304,14 @@ if (portletTitleBasedNavigation) {
 
 						<%@ include file="/wiki/view_page_content.jspf" %>
 
-						<liferay-ui:custom-attributes-available className="<%= WikiPage.class.getName() %>">
-							<liferay-ui:custom-attribute-list
+						<liferay-custom-field-ui:custom-attributes-available className="<%= WikiPage.class.getName() %>">
+							<liferay-custom-field-ui:custom-attribute-list
 								className="<%= WikiPage.class.getName() %>"
 								classPK="<%= (wikiPage != null) ? wikiPage.getPrimaryKey() : 0 %>"
 								editable="<%= false %>"
 								label="<%= true %>"
 							/>
-						</liferay-ui:custom-attributes-available>
+						</liferay-custom-field-ui:custom-attributes-available>
 
 						<c:if test="<%= (wikiPage != null) && Validator.isNotNull(formattedContent) && (followRedirect || (redirectPage == null)) %>">
 							<div class="page-actions">

wiki-web/src/main/resources/META-INF/resources/wiki_admin/page_info_panel.jsp
@@ -149,14 +149,14 @@ if (wikiPageInfoPanelDisplayContext.isSinglePageSelection()) {
 							/>
 						</c:if>
 
-						<liferay-ui:custom-attributes-available className="<%= WikiPage.class.getName() %>">
-							<liferay-ui:custom-attribute-list
+						<liferay-custom-field-ui:custom-attributes-available className="<%= WikiPage.class.getName() %>">
+							<liferay-custom-field-ui:custom-attribute-list
 								className="<%= WikiPage.class.getName() %>"
 								classPK="<%= wikiPage.getResourcePrimKey() %>"
 								editable="<%= false %>"
 								label="<%= true %>"
 							/>
-						</liferay-ui:custom-attributes-available>
+						</liferay-custom-field-ui:custom-attributes-available>
 
 						<%
 						AssetEntry wikiPageAssetEntry = AssetEntryLocalServiceUtil.fetchEntry(WikiPage.class.getName(), wikiPage.getPrimaryKey());