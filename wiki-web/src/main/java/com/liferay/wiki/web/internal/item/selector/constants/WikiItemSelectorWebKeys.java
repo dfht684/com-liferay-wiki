adolfopa/liferay-portal

LPS-72803 Move constants to a class [wiki]

modules/apps/collaboration/wiki/wiki-web/src/main/java/com/liferay/wiki/web/internal/item/selector/view/WikiItemSelectorWebKeys.java
@@ -1,0 +1,29 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.wiki.web.internal.item.selector.view;
+
+/**
+ * @author Roberto Díaz
+ */
+public class WikiItemSelectorWebKeys {
+
+	public static final String
+		WIKI_ATTACHMENT_ITEM_SELECTOR_VIEW_DISPLAY_CONTEXT =
+			"WIKI_ATTACHMENT_ITEM_SELECTOR_VIEW_DISPLAY_CONTEXT";
+
+	public static final String WIKI_PAGE_ITEM_SELECTOR_VIEW_DISPLAY_CONTEXT =
+		"WIKI_PAGE_ITEM_SELECTOR_VIEW_DISPLAY_CONTEXT";
+
+}

modules/apps/collaboration/wiki/wiki-web/src/main/resources/META-INF/resources/item/selector/init.jsp
@@ -53,8 +53,7 @@ page import="com.liferay.wiki.model.WikiNode" %><%@
 page import="com.liferay.wiki.model.WikiPage" %><%@
 page import="com.liferay.wiki.service.WikiPageLocalServiceUtil" %><%@
 page import="com.liferay.wiki.web.internal.item.selector.resolver.WikiPageItemSelectorReturnTypeResolver" %><%@
-page import="com.liferay.wiki.web.internal.item.selector.view.WikiAttachmentItemSelectorView" %><%@
-page import="com.liferay.wiki.web.internal.item.selector.view.WikiPageItemSelectorView" %><%@
+page import="com.liferay.wiki.web.internal.item.selector.view.WikiItemSelectorWebKeys" %><%@
 page import="com.liferay.wiki.web.internal.item.selector.view.display.context.WikiAttachmentItemSelectorViewDisplayContext" %><%@
 page import="com.liferay.wiki.web.internal.item.selector.view.display.context.WikiPageItemSelectorViewDisplayContext" %>
 

dfht684/com-liferay-wiki

wiki-web/src/main/java/com/liferay/wiki/web/internal/item/selector/constants/WikiItemSelectorWebKeys.java
@@ -1,0 +1,29 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.wiki.web.internal.item.selector.constants;
+
+/**
+ * @author Roberto Díaz
+ */
+public class WikiItemSelectorWebKeys {
+
+	public static final String
+		WIKI_ATTACHMENT_ITEM_SELECTOR_VIEW_DISPLAY_CONTEXT =
+			"WIKI_ATTACHMENT_ITEM_SELECTOR_VIEW_DISPLAY_CONTEXT";
+
+	public static final String WIKI_PAGE_ITEM_SELECTOR_VIEW_DISPLAY_CONTEXT =
+		"WIKI_PAGE_ITEM_SELECTOR_VIEW_DISPLAY_CONTEXT";
+
+}

wiki-web/src/main/resources/META-INF/resources/item/selector/init.jsp
@@ -52,9 +52,8 @@ page import="com.liferay.portal.kernel.util.Validator" %><%@
 page import="com.liferay.wiki.model.WikiNode" %><%@
 page import="com.liferay.wiki.model.WikiPage" %><%@
 page import="com.liferay.wiki.service.WikiPageLocalServiceUtil" %><%@
+page import="com.liferay.wiki.web.internal.item.selector.constants.WikiItemSelectorWebKeys" %><%@
 page import="com.liferay.wiki.web.internal.item.selector.resolver.WikiPageItemSelectorReturnTypeResolver" %><%@
-page import="com.liferay.wiki.web.internal.item.selector.view.WikiAttachmentItemSelectorView" %><%@
-page import="com.liferay.wiki.web.internal.item.selector.view.WikiPageItemSelectorView" %><%@
 page import="com.liferay.wiki.web.internal.item.selector.view.display.context.WikiAttachmentItemSelectorViewDisplayContext" %><%@
 page import="com.liferay.wiki.web.internal.item.selector.view.display.context.WikiPageItemSelectorViewDisplayContext" %>
 