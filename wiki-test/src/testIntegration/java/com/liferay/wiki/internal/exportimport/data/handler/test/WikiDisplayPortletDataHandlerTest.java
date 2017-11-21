b33aab6ca0a31bd4c246a7c87b74c8c23cb52912
Gergely Mathe <gergely.mathe@liferay.com>
2017-11-21 15:23:20 +0100

    LPS-76009 Add test for getDataPortletPreferences method

--- wiki-test/src/testIntegration/java/com/liferay/wiki/internal/exportimport/data/handler/test/WikiDisplayPortletDataHandlerTest.java
+++ wiki-test/src/testIntegration/java/com/liferay/wiki/internal/exportimport/data/handler/test/WikiDisplayPortletDataHandlerTest.java
@@ -19,16 +19,20 @@ import com.liferay.exportimport.kernel.lar.DataLevel;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.Sync;
 import com.liferay.portal.kernel.test.rule.SynchronousDestinationTestRule;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.lar.test.BasePortletDataHandlerTestCase;
+import com.liferay.portal.service.test.ServiceTestUtil;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.wiki.constants.WikiPortletKeys;
 
+import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
 
 /**
  * @author Zoltan Csaszi
+ * @author Gergely Mathe
  */
 @RunWith(Arquillian.class)
 @Sync
@@ -42,6 +46,14 @@ public class WikiDisplayPortletDataHandlerTest
 			new LiferayIntegrationTestRule(),
 			SynchronousDestinationTestRule.INSTANCE);
 
+	@Before
+	@Override
+	public void setUp() throws Exception {
+		ServiceTestUtil.setUser(TestPropsValues.getUser());
+
+		super.setUp();
+	}
+
 	@Override
 	protected void addStagedModels() throws Exception {
 	}
@@ -49,6 +61,11 @@ public class WikiDisplayPortletDataHandlerTest
 	@Override
 	protected DataLevel getDataLevel() {
 		return DataLevel.PORTLET_INSTANCE;
+	}
+
+	@Override
+	protected String[] getDataPortletPreferences() {
+		return new String[] {"title", "nodeId"};
 	}
 
 	@Override
